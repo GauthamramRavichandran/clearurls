0d2decea789be43ae6219f86301cfa7fb10879ea
GauthamramRavichandran <30320759+GauthamramRavichandran@users.noreply.github.com>
2020-10-29 22:43:56 +0530

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -48,10 +48,10 @@ async def clearurl_hndlr(event):
 			
 			if url is not None:
 				to_send.append(clear_url(url))
-                if len(to_send) > 0:
+                if to_send:
 		        to_send_txt = "\n\n".join(i for i in to_send)
 		        await event.reply(f"🧹 Cleaned URLs: "
-		                  f"\n{to_send_txt}", link_preview = False)
+		                  	  f"\n{to_send_txt}", link_preview = False)
 	else:
 		chat = await event.get_chat()
 		if isinstance(chat, User):   # don't disturb the group, only throw error at private chat
